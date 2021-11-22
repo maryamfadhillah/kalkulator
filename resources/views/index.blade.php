474233b791c2f3981ac4b5242f475a06ed76efab
Maryam F <maryam@student.uns.ac.id>
2021-11-22 17:00:09 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Kalkulator</title>
+    <link rel="stylesheet" type="text/css" href="">
+</head>
+
+<body>
+<div>
+    <h2>Kalkulator Sederhana</h2>
+    <form method="post" action="index.php">
+    <input type="text" name="bil1">
+    <input type="text" name="bil2">
+    <select name="operasi">
+        <option value="tambah">+</option>
+        <option value="kurang">-</option>
+        <option value="kali">x</option>
+        <option value="bagi">/</option>
+    </select>
+    <input type="submit" name="hitung" value="Hitung">
+    </form>
+</div>
+</body>
+
+</html>
